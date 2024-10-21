e3cd71d31baa5f84bbe62879722cc96f0c97ba1a
Zack Mitchell <Zack.Mitchell@dpr.co.uk>
2024-10-21 17:00:50 +0100

    Initial commit

---
+++ TestProfilingNet9/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TestProfilingNet9
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+
+            Routing.RegisterRoute("SecondPage", typeof(SecondPage));
+            builder.Services.AddTransient<SecondPage>();
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ TestProfilingNet9/SecondPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿namespace TestProfilingNet9
+{
+    public partial class SecondPage : ContentPage, IDisposable
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DisposePage(Microsoft.Maui.Controls.Page? page)
+        {
+            if (page != null)
+            {
+                DisposeChildElements(page);
+                if (page.BindingContext is IDisposable disposableViewModel)
+                {
+                    disposableViewModel.Dispose();
+                }
+                page.BindingContext = null;
+            }
+        }
+
+        private void DisposeChildElements(IView view)
+        {
+            if (view is Layout layout)
+            {
+                foreach (var child in layout.Children)
+                {
+                    DisposeChildElements(child);
+                    if (child is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+            }
+            else if (view is IContentView { Content: View viewContent })
+            {
+                DisposeChildElements(viewContent);
+            }
+
+            if (view is IDisposable disposableView)
+            {
+                disposableView.Dispose();
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            this.Behaviors.Clear();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisposePage(this);
+            Shell.Current.GoToAsync("..");
+            GC.Collect();
+        }
+    }
+
+}
